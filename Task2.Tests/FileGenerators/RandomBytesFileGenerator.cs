27cbcfcb56be5bde6e1dc648ead9951310ec9db6
Alexander Yakusik <alexander.yakusik@gmail.com>
2017-11-28 09:41:13 +0300

    Implemented Task2 and created console example for it.

---
+++ Task2.Tests/FileGenerators/RandomBytesFileGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Task2.Solution;
+
+namespace Task2.Console.FileGenerators
+{
+    public class RandomBytesFileGenerator : AbstractFileGenerator
+    {
+        public override string WorkingDirectory { get; protected set; } = "Files with random bytes";
+
+        public override string FileExtension { get; protected set; } = ".bytes";
+
+        protected override byte[] GenerateFileContent(int contentLength)
+        {
+            var random = new Random();
+
+            var fileContent = new byte[contentLength];
+
+            random.NextBytes(fileContent);
+
+            return fileContent;
+        }
+    }
+}
---
+++ Task2.Tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using Task2.Console.FileGenerators;
+using Task2.Solution;
+
+namespace Task2.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AbstractFileGenerator charGenerator;
+
+            charGenerator = new RandomCharsFileGenerator();
+            
+            charGenerator.GenerateFiles(1, 10);
+
+            charGenerator = new RandomBytesFileGenerator();
+
+            charGenerator.GenerateFiles(10, 15);
+        }
+    }
+}
