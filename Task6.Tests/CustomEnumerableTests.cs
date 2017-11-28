700e331e2210e50a2dd7b8db5a46da9a6e654d5c
Alexander Yakusik <alexander.yakusik@gmail.com>
2017-11-28 11:10:33 +0300

    Implemented Task6 and created tests for it.

--- Task6.Tests/CustomEnumerableTests.cs
+++ Task6.Tests/CustomEnumerableTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Test6.Solution;
 
 namespace Task6.Tests
 {
@@ -16,23 +17,36 @@ namespace Task6.Tests
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(expected, SequenceGenerator.FunctionSequence(
+                expected[0], 
+                expected[1], 
+                ((first, second) => first + second),
+                expected.Length));
         }
 
         [Test]
         public void Generator_ForSequence2()
         {
-            int[] expected = { 1, 2, -10, 92, -796, 6920, -60136, 522608, -4541680, 39469088 };
+            int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(expected, SequenceGenerator.FunctionSequence(
+                expected[0],
+                expected[1],
+                ((first, second) => 6 * first - 8 * second),
+                expected.Length));
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
-            double[] expected = { 1, 2, 3, 3.5, 4.16666666666667, 4.69047619047619, 5.29238095238095, 5.81880106357264, 6.39184849183592, 6.91728310858544 };
+            double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            Assert.That(expected, Is.EqualTo(
+                SequenceGenerator.FunctionSequence(
+                    expected[0],
+                    expected[1],
+                    ((first, second) => first + second / first),
+                    expected.Length)).Within(0.0000000001));
         }
     }
 }
---
+++ Test6.Solution/SequenceGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test6.Solution
+{
+    public static class SequenceGenerator
+    {
+        public static IEnumerable<T> FunctionSequence<T>(
+            T firstElement, 
+            T secondElement, 
+            Func<T, T, T> function, 
+            int amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(amount)} cannot be less than one.");
+            }
+
+            if (function == null)
+            {
+                throw new ArgumentNullException($"{nameof(function)} cannot be null.");
+            }
+
+            return FunctionSequenceCore<T>(firstElement, secondElement, function, amount);
+        }
+
+        private static IEnumerable<T> FunctionSequenceCore<T>(
+            T firstElement, 
+            T secondElement, 
+            Func<T, T, T> function,
+            int amount)
+        {
+            T prePrevious = firstElement;
+            T previous = secondElement;
+
+            yield return firstElement;
+
+            if (amount > 1)
+            {
+                yield return secondElement;
+            }
+
+            for (long i = 2; i < amount; i++)
+            {
+                T current = function(previous, prePrevious);
+
+                yield return current;
+
+                prePrevious = previous;
+                previous = current;
+            }
+        }
+    }
+}
